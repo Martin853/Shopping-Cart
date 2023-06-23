8c1caf809e5e2263cfe3c7113addabc987953083
Martin853 <martin005lena@gmail.com>
2023-06-23 21:14:42 +0200

    Fixed item padding

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Item = (props) => {
   return (
-    <div className="w-full h-64 bg-stone-200 rounded-lg flex justify-center items-center">
-      <div className="w-10/12 mx-auto h-5/6 my-auto flex flex-col gap-2">
+    <div className="w-full h-72  bg-stone-200 rounded-lg flex justify-center items-center">
+      <div className="w-10/12 mx-auto h-full py-4  my-auto flex flex-col gap-2">
         <img
           src="https://designshack.net/wp-content/uploads/placeholder-image.png"
           className="w-full rounded bg-violet-600"
@@ -16,7 +16,7 @@ export const Item = (props) => {
             Price: {props.price}$
           </h1>
         </div>
-        <button className="w-full h-full rounded-lg bg-violet-600 outline-none border-none font-montserrat text-white text-lg select-none hover:bg-violet-700">
+        <button className="w-full h-full  rounded-lg bg-violet-600 outline-none border-none font-montserrat text-white text-lg select-none hover:bg-violet-700">
           Add To Cart
         </button>
       </div>
